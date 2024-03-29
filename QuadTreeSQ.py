6f51340522e08ac639a39e5424ab96cce38ae9a6
LenientLettuce <mh09027@st.habib.edu.pk>
2024-03-29 23:56:00 +0500

    Compression Function (Unoptimized)

--- QuadTreeSQ.py
+++ QuadTreeSQ.py
@@ -1,42 +1,49 @@
 import random as rand
 import numpy as np
 import matplotlib.pyplot as plt
+import math as math
 
 def def_mat (n):
-    output = np.random.randint(2, size=(n,n))
+    output = np.random.randint(100, size=(n,n))
     return output
 
-def quad_tree(matrix,n,rows,columns,depth):
+def quad_tree(matrix,n,rows,columns,threshold):
     allsame = True
+    total = 0
     for i in range(n):
         if allsame == True:
             for j in range(n):
-                if matrix[rows][columns] != matrix[rows + i][columns + j]:
+                total += matrix[rows + i][columns + j]
+                if abs(matrix[rows][columns] - matrix[rows + i][columns + j]) > threshold:
                     allsame = False
                     break
         else:
             break
+    avg = total/n**2
     if allsame:
         for i in range(n):
             for j in range(n):
-                matrix[rows + i][columns + j] += depth
+                matrix[rows + i][columns + j] = math.ceil(avg)
         return
     
     n = n//2
-    topleft = quad_tree(matrix,n,rows,columns,depth+1)
-    topright = quad_tree(matrix,n,rows,columns+n,depth+1)
-    bottomleft = quad_tree(matrix,n,rows+n,columns,depth+1)
-    bottomright = quad_tree(matrix,n,rows+n,columns+n,depth+1)
+    topleft = quad_tree(matrix,n,rows,columns,threshold)
+    topright = quad_tree(matrix,n,rows,columns+n,threshold)
+    bottomleft = quad_tree(matrix,n,rows+n,columns,threshold)
+    bottomright = quad_tree(matrix,n,rows+n,columns+n,threshold)
 
 
-matrix = def_mat(64)
+matrix = def_mat(1024)
 plt.imshow(matrix, cmap="plasma")
 plt.colorbar()
 plt.title("Orginal Matrix Visualization")
 plt.show()
 
-quad_tree(matrix,len(matrix),0,0,0)
-plt.imshow(matrix, cmap="plasma")
-plt.colorbar()
-plt.title("Edited Matrix Visualization")
-plt.show()
+for threshold_mult in range(4,11,1):
+    threshold = 100 * (threshold_mult/20) #100 is the amount of unique pixels in an image
+    quad_tree(matrix,len(matrix),0,0,threshold)
+    plt.imshow(matrix, cmap="plasma")
+    plt.colorbar()
+    plt.title("Edited Matrix Visualization")
+    plt.show()
+
