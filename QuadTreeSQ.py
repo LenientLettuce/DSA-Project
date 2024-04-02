0ec4dc180046c70bbbc0822959dcf541419fd05f
LenientLettuce <mh09027@st.habib.edu.pk>
2024-04-02 11:19:28 +0500

    Did some stuff, check GC for update

--- QuadTreeSQ.py
+++ QuadTreeSQ.py
@@ -2,35 +2,40 @@ import random as rand
 import numpy as np
 import matplotlib.pyplot as plt
 import math as math
+from image_import import *
 
 def def_mat (n):
     output = np.random.randint(100, size=(n,n))
     return output
 
-def img_comp(matrix,n,rows,columns,threshold):
+def img_comp(matrix,r,c,rows,columns,threshold):
     allsame = True
-    total = 0
-    for i in range(n):
+    total = [0,0,0]
+    for i in range(r):
         if allsame == True:
-            for j in range(n):
-                total += matrix[rows + i][columns + j]
-                if abs(matrix[rows][columns] - matrix[rows + i][columns + j]) > threshold:
-                    allsame = False
-                    break
+            for j in range(c):
+                for k in range(total):
+                    total[k] += matrix[rows + i][columns + j][k]
+                    if abs(matrix[rows][columns] - matrix[rows + i][columns + j]) > threshold:
+                        allsame = False
+                        break
         else:
             break
-    avg = total/n**2
+    #Need to find avergae somehow
     if allsame:
-        for i in range(n):
-            for j in range(n):
-                matrix[rows + i][columns + j] = math.ceil(avg)
+        for i in range(r):
+            for j in range(c):
+                for k in range (total):
+                    matrix[rows + i][columns + j][k] = math.ceil(total[k])
         return
     
-    n = n//2
-    topleft = img_comp(matrix,n,rows,columns,threshold)
-    topright = img_comp(matrix,n,rows,columns+n,threshold)
-    bottomleft = img_comp(matrix,n,rows+n,columns,threshold)
-    bottomright = img_comp(matrix,n,rows+n,columns+n,threshold)
+    r = r//2
+    c = c//2
+
+    topleft = img_comp(matrix,r,c,rows,columns,threshold)
+    topright = img_comp(matrix,r,c,rows,columns+c,threshold)
+    bottomleft = img_comp(matrix,r,c,rows+r,columns,threshold)
+    bottomright = img_comp(matrix,r,c,rows+r,columns+c,threshold)
 
 def quad_tree(matrix,n,rows,columns): #Basic QuadTree Function
     allsame = True
@@ -47,24 +52,22 @@ def quad_tree(matrix,n,rows,columns): #Basic QuadTree Function
         return
 
     n = n//2 #if each element is not the same, then the section is divided into 4
-    topleft = quad_tree(matrix,n,rows,columns,threshold)
-    topright = quad_tree(matrix,n,rows,columns+n,threshold)
-    bottomleft = quad_tree(matrix,n,rows+n,columns,threshold)
-    bottomright = quad_tree(matrix,n,rows+n,columns+n,threshold)
+    topleft = quad_tree(matrix,n,rows,columns)
+    topright = quad_tree(matrix,n,rows,columns+n)
+    bottomleft = quad_tree(matrix,n,rows+n,columns)
+    bottomright = quad_tree(matrix,n,rows+n,columns+n)
 
+image=r"dsa_proj\\8kimg.jpg"
+img= read(image)
+show(img,"Original")
 
+print(img)
 
-matrix = def_mat(1024)
-plt.imshow(matrix, cmap="plasma")
-plt.colorbar()
-plt.title("Orginal Matrix Visualization")
-plt.show()
-
-for threshold_mult in range(4,11,1):
-    threshold = 100 * (threshold_mult/20) #100 is the amount of unique pixels in an image
-    img_comp(matrix,len(matrix),0,0,threshold)
-    plt.imshow(matrix, cmap="plasma")
-    plt.colorbar()
-    plt.title("Edited Matrix Visualization")
-    plt.show()
+#for threshold_mult in range(4,11,1):
+#    threshold = 100 * (threshold_mult/20) #100 is the amount of unique pixels in an image
+#    img_comp(matrix,len(matrix),0,0,threshold)
+#    plt.imshow(matrix, cmap="plasma")
+#    plt.colorbar()
+#    plt.title("Edited Matrix Visualization")
+#    plt.show()
 
