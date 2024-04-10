2eaa740299fa1e5aabc987f086262bde25075223
LenientLettuce <mh09027@st.habib.edu.pk>
2024-04-10 21:45:41 +0500

    Basic Function works with any sized matrix

--- QuadTreeSQ.py
+++ QuadTreeSQ.py
@@ -13,8 +13,8 @@ def show(img,title):
     plt.title(title)
     plt.show()
 
-def def_mat (n):
-    output = np.random.randint(100, size=(n,n))
+def def_mat (r,c):
+    output = np.random.randint(100, size=(r,c))
     return output
 
 def create_out_mat(r,c): #Creates a 3D matrix which stores RGB Values
@@ -66,25 +66,39 @@ def img_comp(matrix,r,c,rows,columns,threshold,output_matrix):
     bottomleft = img_comp(matrix,r,c,rows+r,columns,threshold,output_matrix)
     bottomright = img_comp(matrix,r,c,rows+r,columns+c,threshold,output_matrix)
 
-def quad_tree(matrix,n,rows,columns): #Basic QuadTree Function
+def quad_tree(matrix,r,c,rows,columns,threshold): #Basic QuadTree Function
+    if r <= 1 and c <= 1: #Means image is only single pixel
+        return
+    if r <= 1: #Ensures recursion doesn't stop until both rows and columns are compressed
+        r = 2
+    if c <= 1:
+        c = 2
     allsame = True
-    for i in range(n): #Loop checks whether each element within the quad tree is same or not
+    total = 0
+    for i in range(r): #Loop checks whether each element within the quad tree is same or not
         if allsame == True:
-            for j in range(n):
-                if abs(matrix[rows][columns] != matrix[rows + i][columns + j]):
+            for j in range(c):
+                total += matrix[rows + i][columns + j]
+                if abs(matrix[rows][columns] - matrix[rows + i][columns + j]) > threshold:
                     allsame = False
                     break
         else:
             break
 
     if allsame: #if each element is same, then the section is kept as is
+        avg = (total)//(r*c)
+        for i in range(r):
+            for j in range(c):
+                matrix[rows + i][columns + j] = avg
         return
 
-    n = n//2 #if each element is not the same, then the section is divided into 4
-    topleft = quad_tree(matrix,n,rows,columns)
-    topright = quad_tree(matrix,n,rows,columns+n)
-    bottomleft = quad_tree(matrix,n,rows+n,columns)
-    bottomright = quad_tree(matrix,n,rows+n,columns+n)
+    r = r//2
+    c = c//2 
+    #if each element is not the same, then the section is divided into 4
+    topleft = quad_tree(matrix,r,c,rows,columns,threshold)
+    topright = quad_tree(matrix,r,c,rows,columns+c,threshold)
+    bottomleft = quad_tree(matrix,r,c,rows+r,columns,threshold)
+    bottomright = quad_tree(matrix,r,c,rows+r,columns+c,threshold)
 
 def main(image):
     img= read(image)
@@ -107,8 +121,17 @@ def main(image):
     conv_mat_img(output_matrix)#converting output matrix to image and saving it
     print("Complete")
 
+def test_diff_aspect():
+    r = [4,3,1,16,5,5,3,2,1,9,3,5]
+    c = [3,2,1,9,3,5,4,3,1,16,5,5]
+
+    for i in range(len(r)):
+        matrix = def_mat(r[i],c[i])
+        show(matrix, "og")
+        quad_tree(matrix,r[i],c[i],0,0,50)
+        show(matrix, "comp")
+
 #image=r"dsa_proj\\8kimg.jpg"
-image=r"Smoll.jpg"
-main(image)
-
-
+#image=r"Smoll.jpg"
+#main(image)
+test_diff_aspect()
