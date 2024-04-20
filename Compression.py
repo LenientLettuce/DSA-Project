c1a4a1ad157d1d72cddaae312996027cc4d0afa9
LenientLettuce <mh09027@st.habib.edu.pk>
2024-04-20 13:05:11 +0500

    Working Compression

--- Compression.py
+++ Compression.py
@@ -5,82 +5,7 @@ import math as math
 from PIL import Image
 from helper import *
 
-def img_comp(matrix,r,c,rows,columns,threshold,output_matrix):
-    allsame = True
-    total = [0,0,0]
-    if r == 1 or c == 1: #Means one dimension of image is only single pixel
-        if r <= 1 and c <= 1:
-            output_matrix[rows][columns] = matrix[rows][columns]
-        elif r <= 1:
-            for j in range(c):
-                for k in range(0,3):
-                            total[k] += matrix[rows][columns + j][k]
-                            if abs(int(matrix[rows][columns][k]) - int(matrix[rows][columns + j][k])) > threshold[k]:
-                                allsame = False
-                                break
-
-            if allsame:
-                for k in range(0,3):
-                    total[k] = total[k]//c
-                for j in range(c):
-                    output_matrix[rows][columns+j] = total
-            else:
-                for j in range(c):
-                    output_matrix[rows][columns+j] = matrix[rows][columns+j]
-            
-        elif c <= 1:
-            for i in range(r):
-                for k in range(0,3):
-                            total[k] += matrix[rows + i][columns][k]
-                            if abs(int(matrix[rows][columns][k]) - int(matrix[rows + i][columns][k])) > threshold[k]:
-                                allsame = False
-                                break
-
-            if allsame:
-                for k in range(0,3):
-                    total[k] = total[k]//r
-                for i in range(r):
-                    output_matrix[rows+i][columns] = total
-            else:
-                for i in range(r):
-                    output_matrix[rows+i][columns] = matrix[rows+i][columns]
-        return
-    
-    for i in range(r): #Code to check if all pixels in img are within certain threshold
-        if allsame == True:
-            for j in range(c):
-                if allsame == True:
-                    for k in range(0,3):
-                            total[k] += matrix[rows + i][columns + j][k]
-                            if abs(int(matrix[rows][columns][k]) - int(matrix[rows + i][columns + j][k])) > threshold[k]:
-                                allsame = False
-                                break
-                else:
-                    break
-        else:
-            break
-    
-    if allsame: #if they are, it avg their value
-        for k in range(0,3):
-            try:
-                total[k] = total[k]//(r*c)
-            except:
-                total[k] = total[k]
-        for i in range(r):
-            for j in range(c):
-                for k in range (3):
-                    output_matrix[rows + i][columns + j][k] = np.uint8(math.floor(total[k]))
-        return
-    
-
-    r = (r)//2
-    c = (c)//2
-    topleft = img_comp(matrix,r,c,rows,columns,threshold,output_matrix)
-    topright = img_comp(matrix,r,c,rows,columns + c,threshold,output_matrix)
-    bottomleft = img_comp(matrix,r,c,rows + r,columns,threshold,output_matrix)
-    bottomright = img_comp(matrix,r,c,rows + r,columns + c,threshold,output_matrix)
-
-def new_comp(matrix, r, c, row, column,threshold,output_matrix):
+def full_comp(matrix, r, c, row, column,threshold,output_matrix):
     # Base case: if the matrix is 1x1, 1xc, or rx1, return the value at the current position
     total = [0,0,0]
     if r == 1 and c == 1:
@@ -131,14 +56,45 @@ def new_comp(matrix, r, c, row, column,threshold,output_matrix):
     extra_r = r % 2
     half_c = c // 2
     extra_c = c % 2
-    topleft = new_comp(matrix, half_r, half_c, row, column,threshold,output_matrix)
-    topright = new_comp(matrix, half_r, half_c + extra_c, row, column + half_c,threshold,output_matrix)
-    bottomleft = new_comp(matrix, half_r + extra_r, half_c, row + half_r, column,threshold,output_matrix)
-    bottomright = new_comp(matrix, half_r + extra_r, half_c + extra_c, row + half_r, column + half_c,threshold,output_matrix)
+    topleft = full_comp(matrix, half_r, half_c, row, column,threshold,output_matrix)
+    topright = full_comp(matrix, half_r, half_c + extra_c, row, column + half_c,threshold,output_matrix)
+    bottomleft = full_comp(matrix, half_r + extra_r, half_c, row + half_r, column,threshold,output_matrix)
+    bottomright = full_comp(matrix, half_r + extra_r, half_c + extra_c, row + half_r, column + half_c,threshold,output_matrix)
+    return
+
+def depth_compression(matrix, r, c, row, column,depth,req_depth,output_matrix):
+    if depth == req_depth:
+        total = [0,0,0]
+        for i in range(r):
+            for j in range(c):
+                for k in range(0,3):
+                    try:
+                        total[k] += matrix[row + i][column + j][k]
+                    except:
+                        pass
+
+        try:
+            for k in range(0,3):
+                total[k] = np.uint8(total[k]//(r*c))
+            for i in range(r):
+                for j in range(c):
+                    output_matrix[row + i][column + j] = total
+        except:
+            pass
+        return
+    
+    half_r = r // 2
+    extra_r = r % 2
+    half_c = c // 2
+    extra_c = c % 2
+    topleft = depth_compression(matrix, half_r, half_c, row, column,depth+1,req_depth,output_matrix)
+    topright = depth_compression(matrix, half_r, half_c + extra_c, row, column + half_c,depth+1,req_depth,output_matrix)
+    bottomleft = depth_compression(matrix, half_r + extra_r, half_c, row + half_r, column,depth+1,req_depth,output_matrix)
+    bottomright = depth_compression(matrix, half_r + extra_r, half_c + extra_c, row + half_r, column + half_c,depth+1,req_depth,output_matrix)
     return
 
         
-def main(image):
+def mainf(image):
     img= read(image)
     shape = (img.shape)
     print("Image Imported")
@@ -147,17 +103,45 @@ def main(image):
     c = shape[1]
     rows = 0
     columns = 0
-    threshold = [1,1,1]
+    stregth = 25
+    threshold = [stregth,stregth,stregth]
     output_matrix = create_out_mat(r,c) #Creating the matrix which will eventually become the
                                         #compressed image
     print("Output Matrix Created")
     print("Compressing Image")
-    new_comp(img,r,c,rows,columns,threshold,output_matrix)
+    full_comp(img,r,c,rows,columns,threshold,output_matrix)
     print("Image Compressed")
 
     print("Saving Compressed Image")
-    name = "Compressedflower"
+    name = "Compressed8K"
     conv_mat_img(output_matrix,name)#converting output matrix to image and saving it
     print("Complete")
 
-main(r"Images\\flower.jpg")
+def maind(image):
+    img= read(image)
+    shape = (img.shape)
+    print("Image Imported")
+
+    r = shape[0] #Getting the dimensions of the image
+    c = shape[1]
+    rows = 0
+    columns = 0
+    gif = []
+    output_matrix = create_out_mat(r,c) #Creating the matrix which will eventually become the
+                                        #compressed image
+    print("Output Matrix Created")
+    print("Compressing Image")
+    for i in range(0,9):
+        depth_compression(img,r,c,rows,columns,0,i,output_matrix)
+        image = Image.fromarray(output_matrix, mode="RGB")
+        image.save("Images\\Gif\\City" + str(i) + ".jpg")
+        gif.append(image)
+
+    create_gif(gif, "Images\\Gif\\City")  
+    print("Complete")
+
+def create_gif(images,name):
+    first = images[0]
+    first.save(name + ".gif",format = "GIF", append_images = images, save_all = True, duration = 300, loop = 1)
+
+maind(r"Images\\city.jpeg")
