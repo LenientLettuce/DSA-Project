c61b18eb284632cf57d791d7eae693d48401b17f
usufk222 <138148728+usufk222@users.noreply.github.com>
2024-04-24 21:28:36 +0500

    working quadtree access test w/ path

--- quadtree_test.py
+++ quadtree_test.py
@@ -9,7 +9,7 @@ from helper import *
 def new_quad(matrix, rows, cols, row, column, target, path, path_num=1):
     if rows == 1 and cols == 1:
         if matrix[row][column] == target:
-            path[path_num] = [(row, column)]
+            # path[path_num] = [(row, column)]
             path["target"] = target
             return path
         else:
@@ -17,14 +17,14 @@ def new_quad(matrix, rows, cols, row, column, target, path, path_num=1):
     elif rows == 1:
         for j in range(cols):
             if matrix[row][column + j] == target:
-                path[path_num] = [(row, column + j)]
+                # path[path_num] = [(row, column + j)]
                 path["target"] = target
                 return path
         return
     elif cols == 1:
         for i in range(rows):
             if matrix[row + i][column] == target:
-                path[path_num] = [(row + i, column)]
+                # path[path_num] = [(row + i, column)]
                 path["target"] = target
                 return path
         return
@@ -54,28 +54,50 @@ def new_quad(matrix, rows, cols, row, column, target, path, path_num=1):
 
     # Cell-Access base case
     if rows <= 2 and cols <= 2:
+        submatrix = []
+        found = False
         for i in range(rows):
+            row_data = []
             for j in range(cols):
-                if matrix[row + i][column + j] == target:
-                    path[path_num] = matrix[row:row+rows][column:column+cols]
-                    path["target"] = target
-                    return path
+                row_data.append(matrix[row + i][column + j])
+                if matrix[i][j] == target:
+                    found = True
+            submatrix.append(row_data)
+        path[path_num] = submatrix
+        if found:
+            path["target"] = target
+            return path
+
     else:
-        print(matrix[row:row+rows][column:column+cols])
-        path[path_num] = matrix[row:row+rows][column:column+cols]
+        submatrix = []
+        for i in range(rows):
+            row_data = []
+            for j in range(cols):
+                row_data.append(matrix[row + i][column + j])
+            submatrix.append(row_data)
+        path[path_num] = submatrix
     half_r = rows // 2
     extra_r = rows % 2
     half_c = cols // 2
     extra_c = cols % 2
-    topleft = new_quad(matrix, half_r, half_c, row, column,target, path, path_num=1)
-    topright = new_quad(matrix, half_r, half_c + extra_c, row, column + half_c,target, path, path_num=1)
-    bottomleft = new_quad(matrix, half_r + extra_r, half_c, row + half_r, column,target, path, path_num=1)
-    bottomright = new_quad(matrix, half_r + extra_r, half_c + extra_c, row + half_r, column + half_c,target, path, path_num=1)
+    topleft = new_quad(matrix, half_r, half_c, row, column,target, path, path_num + 1)
+    if topleft is not None:
+        return topleft
+    topright = new_quad(matrix, half_r, half_c + extra_c, row, column + half_c,target, path, path_num + 1)
+    if topright is not None:
+        return topright
+    bottomleft = new_quad(matrix, half_r + extra_r, half_c, row + half_r, column,target, path, path_num + 1)
+    if bottomleft is not None:
+        return bottomleft
+    bottomright = new_quad(matrix, half_r + extra_r, half_c + extra_c, row + half_r, column + half_c,target, path, path_num + 1)
+    if bottomright is not None:
+        return bottomright
     return
 
 def init_mat(size):
+
     vals = [i for i in range(1, size + 1)]
-    mat_size = round(64**(1/2))
+    mat_size = round(size**(1/2))
     mat = [[None for i in range(mat_size)] for j in range(mat_size)]
     cur_val = 0
     for i in range(mat_size):
@@ -86,9 +108,17 @@ def init_mat(size):
 
 def main():
     mat = init_mat(64)
-    # for row in mat:
-    #     print(row)
     size = len(mat)
-    print(new_quad(mat, size, size,0 ,0 , 63, {}))
+    path = new_quad(mat, size, size,0 ,0 , 2, {})
+    for k, v in path.items():
+        print(k, ":")
+        print("-----------------")
+        if type(v) is not int:
+            for row in v:
+                print(row)
+        else:
+            print(v, "(found)")
+        print()
+    
 
 main()
