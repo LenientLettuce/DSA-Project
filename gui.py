8f5e8b50dd8897f6ef9270a8866a414e5d6bbb6d
usufk222 <138148728+usufk222@users.noreply.github.com>
2024-05-04 18:53:33 +0500

    main page and frontend downscaling page

--- gui.py
+++ gui.py
@@ -20,6 +20,7 @@ def scaling_page():
     title = tk.Label(page, text="Image Downscaling", font=title_font)
     title.pack(ipady=15, ipadx=10)
 
+    # file selection
     filename = "None"
 
     def select_file():
@@ -29,13 +30,42 @@ def scaling_page():
         filename = file.name.split("/")[-1]
         filename_display.config(text=f"Selected file = {filename}")
 
-    file_select = tk.Button(page, text="Select File", width=15, command=lambda: select_file())
-    file_select.pack()
-    filename_display = tk.Label(page, text=f"Selected file = {filename}")
-    filename_display.pack()
+    file_frame = tk.Frame(page)
+    file_frame.pack(fill="x", pady=5)
+
+    file_select = tk.Button(file_frame, text="Select File", width=15, command=lambda: select_file())
+    file_select.pack(side="left", padx=8)
+    filename_display = tk.Label(file_frame, text=f"Selected file = {filename}")
+    filename_display.pack(side="right", ipady=5, padx=8)
+
+    # image display
+    display_original = tk.Button(page, text="Display Original", width=15)
+    display_original.pack(anchor="w", padx=8, ipady=5)
+
+    # Downscaling frame
+    downscale_frame = tk.Frame(page)
+    downscale_frame.pack(fill="x", pady=5)
+    
+    # scale factor select
+    scale_factor_frame = tk.Frame(downscale_frame)
+    scale_factor_frame.pack(side="left", pady=5, padx=10)
+
+    scale_factor_label = tk.Label(scale_factor_frame, text="Select Scale Factor")
+    scale_factor_label.pack()
     v = tk.IntVar(value=4)
-    tk.Radiobutton(page, text="4", variable=v, value=4).pack()
-    tk.Radiobutton(page, text="8", variable=v, value=8).pack()
+
+    tk.Radiobutton(scale_factor_frame, text="4", variable=v, value=4).pack()
+    tk.Radiobutton(scale_factor_frame, text="8", variable=v, value=8).pack()
+    
+    # run downscaling and display
+    downscale_button = tk.Button(downscale_frame, text="Downscale", width=15)
+    downscale_button.pack(side="right", ipady=5, padx=10)
+
+    # AI upscale and display
+    upscale_button = tk.Button(page, text="AI-based Upscale", width=15)
+    upscale_button.pack(ipady=5)
+
+    space = tk.Label(page, text="").pack(ipady=5)
 
     main_return = tk.Button(page, text="Back to main", width=15, command=lambda: (page.destroy(), main_page()))
     main_return.pack(anchor="sw")
